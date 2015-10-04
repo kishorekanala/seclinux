95f4eaafbe12ec504122e3c97c8d8875c241c31b
Kishore Kanala <kishorekanala@gmail.com>
2015-10-04 19:43:02 +0530

    Debug routines and prints

--- linux-2.6.37.4/arch/arm/boot/compressed/misc.c
+++ linux-2.6.37.4/arch/arm/boot/compressed/misc.c
@@ -38,6 +38,7 @@ extern void error(char *x);
 
 #ifdef CONFIG_CPU_V6
 
+
 static void icedcc_putc(int ch)
 {
 	int status, i = 0x4000000;
@@ -164,6 +165,56 @@ unsigned long free_mem_end_ptr;
 #define arch_error(x)
 #endif
 
+void strreverse(char* begin, char* end) {
+	
+	char aux;
+	
+	while(end>begin)
+	
+		aux=*end, *end--=*begin, *begin++=aux;
+	
+}
+
+char* itoa(int value, char* str, int base) 
+{
+	
+	static char num[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+	
+	char* wstr=str;
+	
+	int sign;
+	
+
+	
+	// Validate base
+	
+	if (base<2 || base>35){ *wstr='\0'; return NULL; }
+	
+
+	
+	// Take care of sign
+	
+	if ((sign=value) < 0) value = -value;
+	
+
+	
+	// Conversion. Number is reversed.
+	
+	do *wstr++ = num[value%base]; while(value/=base);
+	
+	if(sign<0) *wstr++='-';
+	
+	*wstr='\0';
+	
+
+	
+	// Reverse string
+	
+	strreverse(str,wstr-1);
+	
+        return str;
+}
+
 void error(char *x)
 {
 	arch_error(x);
@@ -189,6 +240,8 @@ decompress_kernel(unsigned long output_start, unsigned long free_mem_ptr_p,
 		int arch_id)
 {
 	unsigned char *tmp;
+        unsigned int  size=0;
+        char str[10];
 
 	output_data		= (unsigned char *)output_start;
 	free_mem_ptr		= free_mem_ptr_p;
@@ -199,6 +252,39 @@ decompress_kernel(unsigned long output_start, unsigned long free_mem_ptr_p,
 
 	tmp = (unsigned char *) (((unsigned long)input_data_end) - 4);
 	output_ptr = get_unaligned_le32(tmp);
+
+        /* Check Integrity of Kernel image */
+        size = input_data_end - input_data;
+        putstr(itoa(size, str, 10));
+        putstr("\n");
+        putstr(itoa(input_data[size], str, 16));
+        putstr("\n");
+        putstr(itoa(input_data[size-1], str, 16));
+        putstr("\n");
+        putstr(itoa(input_data[size-2], str, 16));
+        putstr("\n");
+        putstr(itoa(input_data[size-3], str, 16));
+        putstr("\n");
+        putstr(itoa(input_data[size-4], str, 16));
+        putstr("\n");
+        putstr(itoa(input_data[size-5], str, 16));
+        putstr("\n");
+        if (input_data[size] == 0x31)
+          putstr("1 YES\n");
+        else
+          putstr("1 NO\n");
+        if (input_data[size-1] == 0x31)
+          putstr("2 YES\n");
+        else
+          putstr("2 NO\n");
+        if (input_data[size-2] == 0x31)
+          putstr("3 YES\n");
+        else
+          putstr("3 NO\n");
+        if (input_data[size-3] == 0x31)
+          putstr("4 YES\n");
+        else
+          putstr("4 NO\n");
 
 	putstr("Uncompressing Linux...");
 	do_decompress(input_data, input_data_end - input_data,
